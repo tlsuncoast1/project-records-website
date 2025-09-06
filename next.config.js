fba781c5625e2d67858fad230f3d3c9f903d124e
Project Rec <egb18crsla@gmail.com>
2025-09-06 18:43:36 -0400

    chore: remove experimental optimizePackageImports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { optimizePackageImports: ['framer-motion'] }
+  reactStrictMode: true
 };
 module.exports = nextConfig;
